184a9eddddc2e8b9dbd46bc32815775cf4045875
Frankie Rodriguez <rfrankie04@gmail.com>
2021-04-26 09:38:44 -0400

    pushing for leons view

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 const Migrations = artifacts.require("Migrations");
+const 
 const NeptuneDeFiToken = artifacts.require("NeptuneDeFi");
 
-module.exports = function (deployer) {
+module.exports = function (deployer, network, accounts) {
+    
   deployer.deploy(NeptuneDeFiToken);
 };
