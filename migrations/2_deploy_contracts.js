88637b00c10c1102c2946892c09a1d81bc71dc15
git-leon <xleonhunter@gmail.com>
2021-04-26 17:45:44 -0400

    checked out branch for requiring each of the artifcats in the build directory upon executing Compiling your contracts... =========================== > Everything is up to date, there is nothing to compile.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,13 @@
-const Migrations = artifacts.require("Migrations");
-const 
-const NeptuneDeFiToken = artifacts.require("NeptuneDeFi");
+
+// this block invokes `artifacts.require(file)` on each `file` in `./build/contracts` directory
+const contractsDirectory = "../build/contracts/";
+const fs = require('fs');
+fs.readdir(contractsDirectory, (err, files) => {
+  files.forEach(file => {
+    artifacts.require(file)
+})});
 
 module.exports = function (deployer, network, accounts) {
-    
+  
   deployer.deploy(NeptuneDeFiToken);
 };
