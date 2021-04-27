38bc0efe50e07368702607ca1a8e69663ae6a632
git-leon <xleonhunter@gmail.com>
2021-04-27 18:16:29 -0400

    updated deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 
 // this block invokes `artifacts.require(file)` on each `file` in `./build/contracts` directory
 module.exports = function (deployer, network, accounts) {
-  const contractsDirectory = "../build/contracts/";
   const fs = require('fs');
+  const contractsDirectory = "../build/contracts/";
   const neptuneDeFiTokenName = "NeptuneDeFi";
   const neptuneDeFiToken = artifacts.require(neptuneDeFiTokenName);
   fs.readdir(contractsDirectory, (err, files) => {
