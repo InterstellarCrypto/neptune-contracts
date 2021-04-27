49b47cc3adc3daaa5f69ed7e61424be1d7745b88
git-leon <xleonhunter@gmail.com>
2021-04-27 11:16:08 -0400

    updated deploy script

--- contracts/NeptuneDeFi.sol
+++ contracts/NeptuneDeFi.sol
@@ -62,7 +62,7 @@ contract NeptuneDeFi is Context, IERC20, Ownable {
     constructor () public {
         _rOwned[_msgSender()] = _rTotal;
         
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F);
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x5540c944E0AdC325740B9b755D49106150a4C122);
          // Create a uniswap pair for this new token
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
---
+++ deploy-application.sh
@@ -0,0 +1,3 @@
+npm install
+truffle compile
+truffle migrate
